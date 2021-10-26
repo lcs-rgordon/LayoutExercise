065048a88f8be115396391ff54965022693ca1e7
Russell Gordon <rgordon@lcs.on.ca>
2021-10-26 13:23:51 -0400

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Russell Gordon on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
